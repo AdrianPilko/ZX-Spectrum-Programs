4c86bd439c897b958353cd964df8e67233bd4cd1
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2026-01-04 13:54:04 +0000

    added more comments

--- inputMagazine_Issue_1_MC_FramePrint_Dissassembly.asm
+++ inputMagazine_Issue_1_MC_FramePrint_Dissassembly.asm
@@ -1,16 +1,30 @@
-;100 DATA 24,55,1,22,0,0,32,32,32,22,0,0,
-;32,32,32,22,0,0,32,32
-;110 DATA 32,22,0,0,144,145,146,22,0,0,147,
-;148,149,22,0,0,150,151,152,22
-;120 DATA 0,0,153,154,155,22,0,0,156,157,
-;158,22,0,0,159,160,161,58,146,126
-;130 DATA 254,1,1,18,0,40,8,56,4,203,33,
-;24,2,14,0,221,33,147,126,221
-;140 DATA 9,58,137,92,71,62,24,144,221,
-;119,1,60,221,119,7,60,221,119,13,58
-;150 DATA 136,92,71,62,33,144,221,119,2,
-;221, 119,8,221,119,14,221,229,62,2,205
-;160 DATA 1,22,209,1,18,0,205,60,32,201,9913
+; FramePrint LISTING FOR INPUT MAGAZINE "SPEEDING UP YOUR GAMES ROUTINES" using machine code
+10 IF PEEK 23733=127 THEN CLEAR 32399:
+LET B=32400: LET Z=0
+20 IF PEEK 23733=255 THEN CLEAR 65199:
+LET B=65200: LET Z=1
+30 LET T = 0:FOR N=B TO B +129:READ
+A:LET T= T + A:POKE N,A:NEXT N:READ
+A:IF A< >T THEN PRINT "DATA
+ERROR":STOP
+40 IF Z=1 THEN POKE 65258,178: POKE
+65259,254: POKE 65277,179: POKE
+65278,254
+50 SAVE "FramePrint" CODE B,130
+60 STOP
+100 DATA 24,55,1,22,0,0,32,32,32,22,0,0,
+32,32,32,22,0,0,32,32
+110 DATA 32,22,0,0,144,145,146,22,0,0,147,
+148,149,22,0,0,150,151,152,22
+120 DATA 0,0,153,154,155,22,0,0,156,157,
+158,22,0,0,159,160,161,58,146,126
+130 DATA 254,1,1,18,0,40,8,56,4,203,33,
+24,2,14,0,221,33,147,126,221
+140 DATA 9,58,137,92,71,62,24,144,221,
+119,1,60,221,119,7,60,221,119,13,58
+150 DATA 136,92,71,62,33,144,221,119,2,
+221, 119,8,221,119,14,221,229,62,2,205
+160 DATA 1,22,209,1,18,0,205,60,32,201,9913
 
 ;; making use of https://clrhome.org/table/ to manually disassemble
 ;; also reference to http://www.primrosebank.net/computers/zxspectrum/docs/CompleteSpectrumROMDisassemblyThe.pdf
@@ -38,6 +52,17 @@
 	156,157,158,
 	22,0,0,
 	159,160,161,
+
+    ;; It's important where this code is in memory, and that's setup by the BASIC routine that
+	;; loads the DATA statements - the cMachine code is placed at 32400 if the PEEK 23733 returns 127 
+	;; and it sets BASIC variable Z to 0. This is critical because if you read the rest of the text and
+	;; the BASIC code it's clear that thie default routine below is written to run on a 16K ZX Spectrum
+	;; the BASIC code has some more POKE's which change the machine code after it's been read from
+	;; the data statements and this converts it to 48K ZX Spectrum compatible. This happens when
+	;; Z=1 as a result of PEEK 23733 returning 255, in that case B (the BASIC variable) is set to
+	;; 65200 and that is where the machine code gets read into from the DATA if you have a 48K machine
+	;; (very sneaky indeed but cleverly allows compatibility with both versions of ZX Spectrum)
+
 	;;; 159,169,161 is the last of the repeating pattern, and aligns with jump relative 55
 	ld a, ($7e92)    ;58,146,126   ;; 58 dec = 3A hex =  ld a,(nn), byte order reverse to 126 146 = 7e92 = 32402
 	cp 1             ;254,1        ;; 254 dec = FE hex= cp n 
@@ -65,7 +90,7 @@
     ld (ix+13),a     ;221,119,13
 	ld a,($5c88)     ;58, 136,92,  reverse byte order 92,136 becomes $5c88
 	ld b,a           ;71
-	ld a, 33         ;62,33
+	ld a, 33         ;62,33.     I'm assuming 33 is the offset to the next screen line 32 wide??
 	sub b            ;144 
 	ld (ix+2),a      ;221,119,2,
     ld (ix+8),a      ;221,119,8
